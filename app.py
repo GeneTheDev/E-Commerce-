4867c1c489fb68567679373cb8447d0025bdc8df
gene21gene <owens.eugene@yahoo.com>
2023-05-30 19:29:43 -0400

    moving create_app from __init__.py into app.py

--- app.py
+++ app.py
@@ -1,15 +1,60 @@
 from flask import Flask, request, redirect, render_template, url_for, session, flash, abort
 from flask_session import Session
 from flask_debugtoolbar import DebugToolbarExtension
-from forms import RegisterForm, LoginForm, UpdateAccountForm
+from .forms import RegisterForm, LoginForm, UpdateAccountForm
 
 from email_validator import validate_email, EmailNotValidError
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 import psycopg2
 import psycopg2.extras
 
-from . import create_app
-app = create_app()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+
+    app = Flask(__name__, template_folder='templates',
+                static_url_path='/static')
+    app.config['SECRET_KEY'] = '<replace with a secret key>'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///webstore_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    app.debug = True
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'info'
+
+    # Import and register blueprints
+    from .products import products_bp
+    from .auth import auth_bp
+    from .cart import cart_bp
+    from .home import home_bp
+    from .contact import contact_bp
+    app.register_blueprint(products_bp, url_prefix="/")
+    app.register_blueprint(cart_bp, url_prefix="/")
+    app.register_blueprint(auth_bp, url_prefix="/")
+    app.register_blueprint(home_bp, url_prefix="/")
+    app.register_blueprint(contact_bp, url_prefix="/")
+
+    with app.app_context():
+        db.create_all()
+
+    return app
 
 
 def validate_email_address(email):
@@ -29,4 +74,5 @@ def validate_email_address(email):
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
