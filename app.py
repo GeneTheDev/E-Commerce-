8478b571ec689ec094e98a5b26b087d23374ba3c
gene21gene <owens.eugene@yahoo.com>
2023-05-29 23:12:00 -0400

    updating form import statement

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template, url_for, session, flash, abort
 from flask_session import Session
 from flask_debugtoolbar import DebugToolbarExtension
-from forms import RegisterForm, LoginForm, UpdateAccountForm
+from .forms import RegisterForm, LoginForm, UpdateAccountForm
 
 from email_validator import validate_email, EmailNotValidError
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
