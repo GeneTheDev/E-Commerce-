b1e2f02269e90918fba010861b316345e6d10c02
gene21gene <owens.eugene@yahoo.com>
2023-05-29 22:51:39 -0400

    updating allproducts route for filtering

--- products/__init__.py
+++ products/__init__.py
@@ -14,27 +14,43 @@ def product_page():
     return render_template("products.html")
 
 
-PER_PAGE = 8
-
-
 @products_bp.route("/allproducts")
 def all_products():
     search = False
     q = request.args.get('q')
     if q:
         search = True
-
+    PER_PAGE = 8
     page = request.args.get(get_page_parameter(), type=int, default=1)
     offset = (page - 1) * PER_PAGE
-    total_products = Product.query.count()
+
+    products_query = Product.query
+
+    category_filter = request.args.get('category')
+    if category_filter:
+        products_query = products_query.filter(
+            Product.category == category_filter)
+
+    price_filter = request.args.get('price')
+    if price_filter:
+        if price_filter == "asc":
+            products_query = products_query.order_by(Product.price.asc())
+        elif price_filter == "desc":
+            products_query = products_query.order_by(Product.price.desc())
+
+    rating_filter = request.args.get('rating')
+    if rating_filter:
+        if rating_filter == "asc":
+            products_query = products_query.order_by(Product.rating.asc())
+        elif rating_filter == "desc":
+            products_query = products_query.order_by(Product.rating.desc())
+
+    total_products = products_query.count()
     pagination = Pagination(page=page, total=total_products,
                             search=search, record_name='products')
 
-    products = Product.query.order_by(
-        Product.id).offset(offset).limit(PER_PAGE).all()
-    category_filter = request.args.get('category')
-    price_filter = request.args.get('price')
-    rating_filter = request.args.get('rating')
+    products = products_query.offset(offset).limit(PER_PAGE).all()
+
     return render_template('allproducts.html', products=products, category_filter=category_filter, price_filter=price_filter, rating_filter=rating_filter, pagination=pagination, offset=offset)
 
 
