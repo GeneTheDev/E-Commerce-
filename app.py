fa2374e66651a0528278bb5911edf731caa425de
gene21gene <owens.eugene@yahoo.com>
2023-05-29 23:09:07 -0400

    updating form import statement

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, redirect, render_template, url_for, session, flash, abort
 from flask_session import Session
 from flask_debugtoolbar import DebugToolbarExtension
-from .forms import RegisterForm, LoginForm, UpdateAccountForm
+from forms import RegisterForm, LoginForm, UpdateAccountForm
 
 from email_validator import validate_email, EmailNotValidError
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
-import psycopg2  # pip install psycopg2
+import psycopg2
 import psycopg2.extras
 
 from . import create_app
