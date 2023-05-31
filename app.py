e8392d026ba6019cb30aab8c88a52b9da18fbdd6
gene21gene <owens.eugene@yahoo.com>
2023-05-30 21:32:55 -0400

    updating database uri

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def create_app():
                 static_url_path='/static')
     app.config['SECRET_KEY'] = '<replace with a secret key>'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gene:password@127.0.0.1/webstore_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gene:password@127.0.0.1:5432/webstore_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
