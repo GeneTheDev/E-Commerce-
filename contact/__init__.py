b2daf20aa47b370021bf7a3a683018ec5b42d3d0
gene21gene <owens.eugene@yahoo.com>
2023-06-04 18:29:28 -0400

    updating contact_page route

--- contact/__init__.py
+++ contact/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from forms import ContactForm
 
 contact_bp = Blueprint('contact', __name__, template_folder='templates')
 
@@ -6,4 +7,8 @@ contact_bp = Blueprint('contact', __name__, template_folder='templates')
 @contact_bp.route('/contact')
 def contact_page():
     """Contact page"""
-    return render_template("/contact.html")
+    form = ContactForm()
+    if form.validate_on_submit():
+        # Handle form submission
+        pass
+    return render_template("/contact.html", form=form)
