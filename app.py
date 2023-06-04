b41639432bc8e88aea0504232a870e2ef22f28f3
gene21gene <owens.eugene@yahoo.com>
2023-06-04 08:42:04 -0400

    adding path for extensions.py

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
-import os
-from flask import Flask, request, redirect, render_template, url_for, session, flash, abort
-from flask_session import Session
-from flask_debugtoolbar import DebugToolbarExtension
-
-from email_validator import validate_email, EmailNotValidError
-from flask_login import LoginManager, login_user, logout_user, current_user, login_required
-import psycopg2
 import psycopg2.extras
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_session import Session
+import psycopg2
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from email_validator import validate_email, EmailNotValidError
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_bcrypt import Bcrypt
-
+from flask_session import Session
+from flask import Flask, request, redirect, render_template, url_for, session, flash, abort
+import os
 from extensions import db, bcrypt, login_manager
+import sys
+sys.path.insert(0, "/home/gene/web_store/")
 
 
 def create_app():
 
     app = Flask(__name__, template_folder='templates',
                 static_url_path='/static')
-    app.config['SECRET_KEY'] = '<replace with a secret key>'
+    app.config['SECRET_KEY'] = 'secret'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-        os.environ.get('DATABASE_URL', 'postgresql:///webstore_db'))
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL', 'postgresql:///webstore_db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -44,6 +37,7 @@ def create_app():
     from cart import cart_bp
     from home import home_bp
     from contact import contact_bp
+
     app.register_blueprint(products_bp, url_prefix="/")
     app.register_blueprint(cart_bp, url_prefix="/")
     app.register_blueprint(auth_bp, url_prefix="/")
@@ -60,13 +54,10 @@ def validate_email_address(email):
     try:
         # Validate the email address
         valid = validate_email(email)
-
         # Get the normalized email address
         email = valid.email
-
         # Return the normalized email address
         return email
-
     except EmailNotValidError as e:
         # Email is not valid, raise an exception
         raise Exception(str(e))
