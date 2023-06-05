e9e7c8112eddf3f887c6af8b5a9f16da7be9a473
gene21gene <owens.eugene@yahoo.com>
2023-06-04 23:41:22 -0400

    updating url prefix for home

--- app.py
+++ app.py
@@ -42,11 +42,11 @@ def create_app():
     from home import home_bp
     from contact import contact_bp
 
-    app.register_blueprint(products_bp, url_prefix="/")
-    app.register_blueprint(cart_bp, url_prefix="/")
-    app.register_blueprint(auth_bp, url_prefix="/")
+    app.register_blueprint(products_bp)
+    app.register_blueprint(cart_bp)
+    app.register_blueprint(auth_bp)
     app.register_blueprint(home_bp, url_prefix="/")
-    app.register_blueprint(contact_bp, url_prefix="/")
+    app.register_blueprint(contact_bp)
 
     with app.app_context():
         db.create_all()
